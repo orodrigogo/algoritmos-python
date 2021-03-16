7785137a1a974f35b34366394745ae18374555cc
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-16 08:23:40 -0300

    Create dobro_triplo.py

---
+++ 006 - Atividades/dobro_triplo.py
@@ -0,0 +1,21 @@
+"""
+    Crie um algoritmo que leia um número e 
+    mostre o seu dobro e seu triplo.
+"""
+
+# Exemplo 1
+numero = int(input("Digite o número: "))
+print(f'Resultado do dobro: {numero * 2}') 
+print(f'Resultado do triplo: {numero * 3}')
+
+
+# Exemplo 2
+dobro = (numero * 2)
+triplo = (numero * 3)
+print(f'O dobro do {numero} é {dobro} e o triplo do {numero} é {triplo}')
+
+
+
+
+
+

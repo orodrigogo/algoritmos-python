fcd258dc9d1dc1c8465329d36b2b1a9276f806ad
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-16 08:24:09 -0300

    Create media.py

---
+++ 006 - Atividades/media.py
@@ -0,0 +1,17 @@
+# Pergunta o nome da disciplina
+disciplina = input("Qual é a disciplina: ")
+
+# Coleto a primeira nota.
+nota1 = float(input("Digite a primeira nota: "))
+
+# Coleto a segunda nota.
+nota2 = float(input("Digite a segunda nota: "))
+
+# Calcula a média
+media = (nota1 + nota2) / 2
+
+# Escrever na tela a média
+print(f"A média da disciplina {disciplina} é {media:.2}")
+
+
+

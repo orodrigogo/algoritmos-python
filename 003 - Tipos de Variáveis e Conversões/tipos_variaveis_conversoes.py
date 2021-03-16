c485b8b87ea6d0589e33d659a6765192dc9ee336
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-16 08:15:55 -0300

    Create tipos_variaveis_conversoes.py

---
+++ 003 - Tipos de Variáveis e Conversões/tipos_variaveis_conversoes.py
@@ -0,0 +1,13 @@
+# Por padrão, o input captura o valor digitado pelo usuário como texto.
+valor = input('Você gosta de coca? ')
+print(f'O Número {valor} é do tipo primitivo {type(valor)}.')
+
+# Para INTEIRO
+valor = int(input('Digite um número inteiro: '))
+print(f'O Número {valor} é do tipo primitivo {type(valor)}.')
+
+# Para FLOAT
+valor = float(input('Digite um número float: '))
+print(f'O Número {valor} é do tipo primitivo {type(valor)}.')
+
+

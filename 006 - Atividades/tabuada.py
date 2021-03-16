dca2405e9668cad05ffb2e0d32810f1a628770df
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-16 08:24:51 -0300

    Create tabuada.py

---
+++ 006 - Atividades/tabuada.py
@@ -0,0 +1,21 @@
+"""
+    Faça um programa que leia um número inteiro 
+    qualquer e mostre na tela a sua tabuada.
+"""
+numero = int(input("Digite o número da tabuada: "))
+print("### TABUADA ###")
+print(f""" 
+    {numero} x 1 = {numero * 1} 
+    {numero} x 2 = {numero * 2}
+    {numero} x 3 = {numero * 3}
+    {numero} x 4 = {numero * 4}
+    {numero} x 5 = {numero * 5}
+    {numero} x 6 = {numero * 6}
+    {numero} x 7 = {numero * 7}
+    {numero} x 8 = {numero * 8}
+    {numero} x 9 = {numero * 9}
+    {numero} x 10 = {numero * 10}
+""")
+
+
+

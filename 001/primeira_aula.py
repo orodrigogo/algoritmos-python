dc4ac6f6ad81fa7b966a5ec6a935d5b1cb91785f
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-16 08:07:24 -0300

    Create primeira_aula.py

---
+++ 001/primeira_aula.py
@@ -0,0 +1,20 @@
+# Isto é um comentário de uma linha.
+
+"""
+    Isto é um comentário
+    de várias linhas.
+"""
+
+# Armazenando valor em variável.
+mensagem = "1"
+
+# Escrevendo na tela o conteúdo de mensagem
+print(mensagem)
+mensagem = "2"
+
+print(mensagem)
+
+# Escreve na tela o nome digitado pelo usuário.
+print(input("Digite o seu nome: "))
+
+

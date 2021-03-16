7854434df6080cd32d10b32a7d439fe039d6ac7c
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-16 08:18:21 -0300

    Create atividade_2.py

---
+++ 004 - Atividades/atividade_2.py
@@ -0,0 +1,10 @@
+# Captura o dia, mês e ano de nascimento do usuário separado.
+dia = input("Que dia você nasceu: ")
+mes = input("Que mês você nasceu: ")
+ano = input("Que ano você nasceu: ")
+
+# Concatena a data de nascimento do usuário em uma frase e armazena na variavel nascimento.
+nascimento = f"Você nasceu no dia {dia} de {mes} de {ano}. Correto?"
+
+# Escreve na tela o conteúdo da variável nascimento.
+print(nascimento)

205ad2b4992097cc93bf76e672ac25fc0b85fdb2
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-03-23 07:50:19 -0300

    Add files via upload

---
+++ 005 - Operadores Aritméticos/operadores_aritmeticos_continuacao.py
@@ -0,0 +1,13 @@
+n1 = int(input("Digite o PRIMEIRO número: "))
+n2 = int(input("Digite o SEGUNDO número: "))
+
+print(f"Resto de Divisão: {n1 % n2}")
+print(f"Divisão Inteira: {n1 // n2}")
+print(f"Potência: {n1 ** n2}")
+
+"""
+    print(f"Soma: {n1 + n2}")
+    print(f"Subtração: {n1 - n2}")
+    print(f"Divisão: {n1 / n2}")
+    print(f"Multiplicação: {n1 * n2}")
+"""

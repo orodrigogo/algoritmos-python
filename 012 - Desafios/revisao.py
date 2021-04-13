a1c1071a40e6dbf3a13af2274ca41e1d809daa92
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-04-12 22:40:08 -0300

    Create revisao.py

---
+++ 012 - Desafios/revisao.py
@@ -0,0 +1,44 @@
+# Entrada de Dados, Processamento e Saída.
+nome = input("Digite o seu nome: ")
+
+print(f"Nome informado: {nome}")
+
+# Tipos de dados
+# str = Texto
+# float = número com casa décimal
+# int = número inteiro
+# bool = verdadeiro ou falso
+
+# Estrutura de Decisão / Estrutura de Condição
+
+# if = SE
+if nome == "rodrigo":
+    print("Faça alguma coisa...")
+else:
+    print("Faça tal coisa...")
+
+if nome == "rodrigo":
+    print("nome bacana")
+elif not nome == "rodrigo":
+    print("Seu nome não é rodrigo")
+elif nome == "joão":
+    print("Que legal, vc tem um nome biblíco")
+elif nome == "Leonardo":
+    print("Você nome de um Power Ranger")
+else:
+    print("Eita, seu nome é único!")
+
+
+# operadores lógicos
+# and = e
+# or = ou
+
+# operadores aritméticos
+# > Maior
+# < Menor
+# != Diferente
+# >= Maior ou Igual
+# <= Menor ou Igual
+
+
+

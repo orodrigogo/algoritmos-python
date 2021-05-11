260836876046c0593bdbc4fed926432e0ad1a81b
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-05-10 21:52:15 -0300

    Add files via upload

---
+++ 014 - Tuplas/dia_semana.py
@@ -0,0 +1,13 @@
+daysOfWeek = (
+    "Invalid Day",
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado"
+)
+
+day = int(input("Digite um número para o dia da semana: "))
+print(daysOfWeek[day])

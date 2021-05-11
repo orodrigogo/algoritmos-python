b8993d44345bded0e3d07a3e6d938656158684e0
Rodrigo Gonçalves S <49030804+rodrigorgtic@users.noreply.github.com>
2021-05-10 21:51:54 -0300

    Create tupla.py

---
+++ 014 - Tuplas/tupla.py
@@ -0,0 +1,15 @@
+# Tupla é uma lista imutável.
+
+fruits = ("Apple", "Banana", "Watermelon", "Grape")
+
+selected = fruits[1]
+print(selected)
+print(f"The list have {len(fruits)} items")
+
+"""
+    Atividade
+    Utilizar uma Tupla com os dias da semana
+     para dizer para o usuário que dia da semana
+    o número informado se refere (1 até 7).
+"""
+
